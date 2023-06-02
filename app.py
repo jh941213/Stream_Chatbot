1766ec502cb6b3e145bfe3ca3ee8036c5e186540
JaeHyun KIM <112835087+jh941213@users.noreply.github.com>
2023-06-02 13:06:39 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("꿀맛빌리지 ChatBot made by 김덕배 ")
+st.title("꿀맛빌리지 ChatBot made by 김덕배")
 st.write("""
 [챗봇으로 이동](https://www.chatbase.co/chatbot/O38c1b-sg0utSeWAJW0EZ)
 """)
